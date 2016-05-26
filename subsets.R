cfd90e7383e107460529457e6eeaf1142a90ed1f
Daniel Stubbs <daniel.stubbs@calculquebec.ca>
2016-05-26 09:51:02 -0400

    Added further possible solutions to the exercise.

--- subsets.R
+++ subsets.R
@@ -2,6 +2,13 @@
 library(datasets)
 data(airquality)
 
+# Il y a plusieurs moyens d'y arriver:
+mean(airquality$Solar.R[airquality$Ozone > 31 & airquality$Temp > 90],na.rm=TRUE)
+
+# Avec la fonction "subset" : pourquoi n'a-t-on pas besoin d'ajouter "na.rm=TRUE" 
+# dans ce cas?
+mean(subset(airquality,airquality$Ozone > 31 & airquality$Temp > 90)$Solar.R)
+
 # Calculer le vecteur de filtrage pour le sous-ensemble 
 filter.vector <- airquality[,"Ozone"] > 31 & airquality[,"Temp"] > 90
 
